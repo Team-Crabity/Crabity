3edbcc072c42af3ed6dd2fde091bd7062095b84f
lukaslicon <lukas@licons.com>
2024-03-16 21:46:25 -0700

    Revert "arabic pause menu"
    This reverts commit 45098bd6efa2b912066e06008ac59c1a94b67263.

--- Assets/Scripts/Localization/LocaleDropdown.cs
+++ Assets/Scripts/Localization/LocaleDropdown.cs
@@ -9,9 +9,6 @@ public class LocaleDropdown : MonoBehaviour
 {
     public TMP_Dropdown dropdown;
 
-    // Public variable to assign the image in the Unity Editor
-    public Sprite defaultLocaleImage;
-
     IEnumerator Start()
     {
         DontDestroyOnLoad(gameObject);
@@ -27,11 +24,11 @@ public class LocaleDropdown : MonoBehaviour
             var locale = LocalizationSettings.AvailableLocales.Locales[i];
             if (LocalizationSettings.SelectedLocale == locale)
                 selected = i;
-
-            // Add the option with the locale name and default image
-            options.Add(new TMP_Dropdown.OptionData(locale.name, defaultLocaleImage));
+            options.Add(new TMP_Dropdown.OptionData(locale.name));
+            Debug.Log("Options: " + options);
         }
         dropdown.options = options;
+        Debug.Log(options);
 
         dropdown.value = selected;
         dropdown.onValueChanged.AddListener(LocaleSelected);
