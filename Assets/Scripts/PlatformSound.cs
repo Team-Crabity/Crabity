e8cefe8195b7d9ed4ec166abd6207b96d542178a
Akhrxr <130079398+Akhrxr@users.noreply.github.com>
2024-03-19 17:49:26 -0700

    Added playercollision check, setup for platform audio and retagged EVERYTHING(kinda)

---
+++ Assets/Scripts/PlatformSound.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformSound : MonoBehaviour
+{
+    public List<AudioClip> pipeSound;
+    public List<AudioClip> boxSound;
+    public List<AudioClip> brickSound;
+    public List<AudioClip> floorSound; //floors and walls
+    private AudioSource source;
+
+    private FSMaterial TagOfMat;
+
+    enum FSMaterial
+    {
+        Pipe,Box,Floor,Brick,Empty
+    }
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        Debug.Log(TagOfMat);
+    }
+
+    void PlayFootStep()
+    {
+        AudioClip clip = null;
+        switch (TagOfMat)
+        {
+            case FSMaterial.Pipe:
+                clip = pipeSound[Random.Range(0,pipeSound.Count)];
+                break;
+            case FSMaterial.Floor:
+                clip = floorSound[Random.Range(0,floorSound.Count)];
+                break;
+            case FSMaterial.Box:
+                clip = boxSound[Random.Range(0,boxSound.Count)];
+                break;
+            case FSMaterial.Brick:
+                clip = brickSound[Random.Range(0,brickSound.Count)];
+                break;
+            default:
+                break;
+        }
+        Debug.Log(TagOfMat);
+        /*if (TagOfMat != FSMaterial.Empty)
+        {
+            source.clip = clip;
+            source.volume = Random.Range(0.02f, 0.05f);
+            source.pitch = Random.Range(0.8f, 1.2f);
+            source.Play();
+        }*/
+    }
+
+    void OnCollisionEnter (Collision col)
+    {
+        if (col.gameObject.tag == "Ground") 
+        {
+            TagOfMat = FSMaterial.Floor;
+        }
+        else if (col.gameObject.tag == "Pipe") 
+        {
+            TagOfMat = FSMaterial.Pipe;
+        }
+        else if (col.gameObject.tag == "Wood") 
+        {
+            TagOfMat = FSMaterial.Box;
+        }
+        else if (col.gameObject.tag == "Brick") 
+        {
+            TagOfMat = FSMaterial.Brick;
+        }
+        else
+        {
+            TagOfMat = FSMaterial.Empty;
+        }
+    }
+
+    void OnCollisionExit () 
+    {
+        TagOfMat = FSMaterial.Empty;
+    }
+
+
+}
