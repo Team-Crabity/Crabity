0e97b01b8542686e78997487dbc858a0d90ed2a2
Akhrxr <130079398+Akhrxr@users.noreply.github.com>
2024-04-18 18:31:33 -0700

    SettingsMenu completed. I really hope this commit does not mess anything up

---
+++ Assets/SlimUI/Modern Menu 1/Scripts/Managers/UIMenuManager.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+namespace SlimUI.ModernMenu{
+	public class UIMenuManager : MonoBehaviour {
+
+        public enum Theme {custom1, custom2, custom3};
+        [Header("THEME SETTINGS")]
+        public Theme theme;
+        private int themeIndex;
+        public ThemedUIData themeController;
+
+        [Header("PANELS")]
+        [Tooltip("The UI Panel that holds the VIDEO window tab")]
+        public GameObject PanelVideo;
+        [Tooltip("The UI Panel that holds the Audio window tab")]
+        public GameObject PanelGame;
+
+		[Tooltip("Door Opener")]
+		public GameObject door;
+		public GameObject SettingsMenu;
+		public GameObject Cam; // The camera object
+        
+
+        // highlights in settings screen
+        [Header("SETTINGS SCREEN")]
+        [Tooltip("Highlight Image for when GAME Tab is selected in Settings")]
+        public GameObject lineGame;
+        [Tooltip("Highlight Image for when VIDEO Tab is selected in Settings")]
+        public GameObject lineVideo;
+
+		[Header("SFX")]
+        [Tooltip("The GameObject holding the Audio Source component for the HOVER SOUND")]
+        public AudioSource hoverSound;
+        [Tooltip("The GameObject holding the Audio Source component for the AUDIO SLIDER")]
+        public AudioSource sliderSound;
+        [Tooltip("The GameObject holding the Audio Source component for the SWOOSH SOUND when switching to the Settings Screen")]
+        public AudioSource swooshSound;
+
+		void Start(){
+			SetThemeColors();
+		}
+
+
+		void SetThemeColors()
+		{
+			switch (theme)
+			{
+				case Theme.custom1:
+					themeController.currentColor = themeController.custom1.graphic1;
+					themeController.textColor = themeController.custom1.text1;
+					themeIndex = 0;
+					break;
+				case Theme.custom2:
+					themeController.currentColor = themeController.custom2.graphic2;
+					themeController.textColor = themeController.custom2.text2;
+					themeIndex = 1;
+					break;
+				case Theme.custom3:
+					themeController.currentColor = themeController.custom3.graphic3;
+					themeController.textColor = themeController.custom3.text3;
+					themeIndex = 2;
+					break;
+				default:
+					Debug.Log("Invalid theme selected.");
+					break;
+			}
+		}
+
+		void DisablePanels(){
+			PanelVideo.SetActive(false);
+			PanelGame.SetActive(false);
+
+			lineGame.SetActive(false);
+			lineVideo.SetActive(false);
+		}
+
+		public void Return()
+		{
+			SettingsMenu.SetActive(false);
+			Cam.transform.position = new Vector3(0.35f, 0.8f, 3.94f);
+		}
+		public void GamePanel(){
+			DisablePanels();
+			PanelGame.SetActive(true);
+			lineGame.SetActive(true);
+		}
+
+		public void VideoPanel(){
+			DisablePanels();
+			PanelVideo.SetActive(true);
+			lineVideo.SetActive(true);
+		}
+
+
+		public void PlayHover(){
+			hoverSound.Play();
+		}
+
+		public void PlaySFXHover(){
+			sliderSound.Play();
+		}
+
+		public void PlaySwoosh(){
+			swooshSound.Play();
+		}
+	}
+}
