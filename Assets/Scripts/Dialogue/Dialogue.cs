77f3f8f9381672e23d909b16cb3ff608e0e4341b
jmreavis <131843275+jmreavis@users.noreply.github.com>
2024-04-24 22:07:53 -0700

    Tweaked script for mac

--- Assets/Scripts/Dialogue/Dialogue.cs
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -11,7 +11,6 @@ public class Dialogue : MonoBehaviour
     private int index;
     public AudioSource source;
     public AudioClip clip;
-    PlayerManager player;
     private string[] lines = new string[]
     {
 
@@ -19,8 +18,7 @@ public class Dialogue : MonoBehaviour
 
     void Start()
     {
-        player = FindObjectOfType<PlayerManager>();
-        if (player.CompanionMode == false) //single player dialogue
+        if (PlayerManager.instance.CompanionMode == false) //single player dialogue
         {
             lines = new string[]
             {
@@ -28,9 +26,9 @@ public class Dialogue : MonoBehaviour
                 "Initiating contact...done.",
                 "Greetings, CRB 1. Our records indicate this facility should be abandoned. Commence escape protocol.",
                 "Preliminary scans reveal corrupt memory data: reinitializing training module... done.",
-                "CRB 1: Use the A and D keys to move left and right; use the W key to jump.",
+                "CRB 1: Use the W, A, S and D keys to navigate the environment.",
                 "Utilize the rotation drive with LEFT SHIFT + WASD.",
-                "Utilize the gravity drive with C + WASD.",
+                "Utilize the gravity drive with LEFT CTRL + WASD.",
             };
         }
         else //coop dialogue
@@ -41,11 +39,26 @@ public class Dialogue : MonoBehaviour
                 "Initiating contact...done.",
                 "Greetings, CRB 1 and 2. Our records indicate this facility should be abandoned. Commence escape protocol.",
                 "Preliminary scans reveal corrupt memory data: reinitializing training module... done.",
-                "CRB 1: Use the A and D keys to move left and right; use the W key to jump.", 
+                "CRB 1: Use the W, A, S and D keys to navigate the environment.", 
                 "CRB 1: Utilize the rotation drive with LEFT SHIFT + WASD.",
-                "CRB 2: Use the LEFT and RIGHT arrow keys to move; use the UP arrow key to jump.",
+                "CRB 2: Use the LEFT, RIGHT, UP, AND DOWN arrow keys to navigate the environment.",
                 "CRB 2: Utilize the gravity drive with RIGHT CTRL + ARROW KEYS.",
             };
+        }
+
+        if(SystemInfo.operatingSystemFamily == OperatingSystemFamily.MacOSX) // Mac edge case
+        {
+            lines = new string[]
+           {
+                "Tip: advance this dialogue by using either SPACE or ENTER.",
+                "Initiating contact...done.",
+                "Greetings, CRB 1 and 2. Our records indicate this facility should be abandoned. Commence escape protocol.",
+                "Preliminary scans reveal corrupt memory data: reinitializing training module... done.",
+                "CRB 1: Use the W, A, S and D keys to navigate the environment.",
+                "CRB 1: Utilize the rotation drive with LEFT SHIFT + WASD.",
+                "CRB 2: Use the LEFT, RIGHT, UP, AND DOWN arrow keys to navigate the environment.",
+                "CRB 2: Utilize the gravity drive with RIGHT OPTIONS + ARROW KEYS.",
+           };
         }
         textComponent.text = string.Empty;
         StartDialogue();
