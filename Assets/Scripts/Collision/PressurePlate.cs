911aad5739e3783eebff8ed35768253218401ac5
Jentsen <jentsenmaniti@gmail.com>
2024-04-27 22:34:58 -0700

    added tab hotkey and temp button for map + lerp effect

--- Assets/Scripts/Collision/PressurePlate.cs
+++ Assets/Scripts/Collision/PressurePlate.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PressurePlate : MonoBehaviour
 {
     private bool isPressed;
+    public Material baseColor;
 
     void OnCollisionEnter(Collision other)
     {
@@ -16,16 +17,12 @@ public class PressurePlate : MonoBehaviour
             GetComponent<AudioSource>().Play();
             transform.localScale = new Vector3(transform.localScale.x, 0.15f, transform.localScale.z);
             isPressed = true;
-
-            // GetComponent<Collider>().enabled = false;
-            // Material remover curerntly not working
-            // Material[] materials = new Material[renderer.materials.Length - 1];
-
-            // for (int i = 0; i < materials.Length; i++)
-            // {
-            //     materials[i-1] = renderer.materials[i];
-            // }
-            // renderer.materials = materials;
+            // Change the pressure plate material element 0 to base color
+            if (renderer.materials.Length > 0)
+            {
+                Debug.Log("Changing material color");
+                renderer.materials[0] = baseColor;
+            }
 
             PressurePlateManager.instance.PlatePressed();
         }
---
+++ Assets/Scripts/Menus/Map.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour
+{
+    public static MapManager instance;
+    
+    [SerializeField]
+    private GameObject border;
+    [SerializeField]
+    private GameObject map;
+    [SerializeField]
+    private float duration = 0.1f;
+
+    private float targetWidth;
+    private float currentWidth;
+    private bool mapOpening = false;
+    private PauseMenu pauseMenu;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            // Toggle map on and off with slerp effect
+            ToggleMap();
+        }       
+    }
+
+    public void ToggleMap()
+    {
+        StartCoroutine(SlerpMap());
+    }
+
+    public IEnumerator SlerpMap()
+    {
+        if (map == null)
+        {
+            map = GameObject.Find("Map");
+        }
+
+        if(!mapOpening)
+        {
+            mapOpening = true;
+            // Check if map is minimized or not, if not then set targetWidth to 0
+            if (map.transform.localScale.x == 0)
+            {
+                targetWidth = 1;
+            }
+            else
+            {
+                targetWidth = 0;
+            
+            }
+
+            float time = 0;
+            float currentWidth = map.transform.localScale.x;
+            
+            while (time < duration)
+            {
+                time += Time.deltaTime;
+                float t = time / duration;
+                float newWidth = Mathf.Lerp(currentWidth, targetWidth, t);
+                // Map pop in horizontally
+                map.transform.localScale = new Vector3(newWidth, map.transform.localScale.y, map.transform.localScale.z);
+                // Border pop in vertically
+                border.transform.localScale = new Vector3(border.transform.localScale.x, newWidth, border.transform.localScale.z);
+                yield return null;
+            }
+            mapOpening = false;
+        }
+    }
+}
