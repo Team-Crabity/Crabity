4fa6a4a1774cff13f7e7ef07a4b5fb1957e99cbb
Ernani Raffo <ernani.raffo@icloud.com>
2024-05-11 21:14:27 -0700

    Added Unity Analytics. Sends event when completing a level.

--- Assets/Scripts/Collision/Door.cs
+++ Assets/Scripts/Collision/Door.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Unity.Services.Core;
+using Unity.Services.Analytics;
 
 public class Door : MonoBehaviour
 {
@@ -24,7 +26,14 @@ public class Door : MonoBehaviour
         yield return new WaitForSeconds(delay);
         Scene s = SceneManager.GetActiveScene();
         Debug.Log("Going to scene: " + (s.buildIndex + 1));
+        
+        // send event
+        CustomEvent myEvent = new CustomEvent("levelComplete")
+        {
+            {"levelName", s.name},
+        };
+        AnalyticsService.Instance.RecordEvent(myEvent);
+
         SceneManager.LoadScene(s.buildIndex + 1);
     }
-
 }
---
+++ Assets/Scripts/Managers/AnalyticsManager.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Services.Core;
+using Unity.Services.Analytics;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class AnalyticsManager : MonoBehaviour
+{
+    // canvas
+    Canvas canvas;
+
+    // buttons
+    Button[] buttons;
+
+    async void Start()
+    {
+        // disable canvas element
+        canvas = GetComponentInChildren<Canvas>();
+        canvas.enabled = false;
+
+        // get buttons and assign callbacks
+        buttons = GetComponentsInChildren<Button>();
+        for (int i = 0; i < buttons.Length; i++) {
+            if (buttons[i].GetComponentInChildren<TMP_Text>().text == "Yes") {
+                buttons[i].onClick.AddListener(OnAgree);
+            } else {
+                buttons[i].onClick.AddListener(OnDisagree);
+            }
+        }
+
+		await UnityServices.InitializeAsync();
+
+		AskForConsent();
+    }
+
+	void AskForConsent()
+	{
+		// ... show the player a UI element that asks for consent.
+        canvas.enabled = true;
+	}
+
+    void OnAgree() {
+        Debug.Log("User agreed to share data");
+
+        // start collecting data
+        ConsentGiven();
+
+        // disable canvas
+        canvas.enabled = false;
+
+        MainMenu();
+    }
+
+    void OnDisagree() {
+        // disable canvas
+        canvas.enabled = false;
+    }
+
+	void ConsentGiven()
+	{
+		AnalyticsService.Instance.StartDataCollection();
+        Debug.Log("Collecting Data from User");
+        
+        MainMenu();
+	}
+
+    void MainMenu() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
