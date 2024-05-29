cc258feb6d4115fd3ecee44050e947e9acc2f208
jmreavis <131843275+jmreavis@users.noreply.github.com>
2024-05-29 00:03:07 -0700

    alluded to breaking glass mechanic via dialogue

--- Assets/Scripts/Dialogue/Laser_Dialogue.cs
+++ Assets/Scripts/Dialogue/Laser_Dialogue.cs
@@ -21,7 +21,8 @@ public class Laser_Dialogue : MonoBehaviour
     {
         lines = new string[]
         {
-            "WARNING: Unauthorized sector access; initiating laser security prototols."
+            "WARNING: Unauthorized sector access; initiating laser security prototols.",
+            "Severe structural damage detected. Be wary of damaged components, especially glass."
         };
         textComponent.text = string.Empty;
         StartDialogue();
