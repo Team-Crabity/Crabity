4cf5b167cca197b51924a7d3a048657db58b0a96
Jentsen <jentsenmaniti@gmail.com>
2024-05-29 00:00:18 -0700

    added new art assets for crabs + idle animation, changed assets for lasers + added animation

--- Assets/Scripts/Mechanics/LaserTrap.cs
+++ Assets/Scripts/Mechanics/LaserTrap.cs
@@ -11,6 +11,7 @@ public class LaserTrap : MonoBehaviour
     [SerializeField] public GameObject laser; // Reference to the existing laser GameObject
     public float laserLifetime = 2.0f; // Duration for which the laser stays active
     public float respawnInterval = 3.0f; // Time interval between each laser respawn
+    public float animationDuration = 0.15f; // Duration for the laser to animate
 
     private Vector3 initialPosition; // Initial position of the laser
     private Quaternion initialRotation; // Initial rotation of the laser
@@ -35,10 +36,10 @@ public class LaserTrap : MonoBehaviour
     {
         while (true)
         {
-            // Activate the laser and reset its position and rotation
-            //laser.transform.localPosition = initialPosition;
-            //laser.transform.localRotation = initialRotation;
-            laser.SetActive(true);
+            // Activate laser
+            // laser.SetActive(true);
+            StartCoroutine(AnimateLaser(Vector3.zero, Vector3.one, animationDuration));
+
             Source.clip = IdleSound;
             Source.volume = (0.7f);
             Source.Play();
@@ -47,13 +48,34 @@ public class LaserTrap : MonoBehaviour
             // Wait for the laser lifetime duration
             yield return new WaitForSeconds(laserLifetime);
 
-            // Deactivate the laser
+            // Deactivate the laser + moves it to hidden
+            // laser.SetActive(false);
+            StartCoroutine(AnimateLaser(new Vector3(0, -3.2f, 0), new Vector3(1, 0, 1), animationDuration));
             Source.Stop();
             Source.loop = false;
-            laser.SetActive(false);
 
             // Wait for the respawn interval
             yield return new WaitForSeconds(respawnInterval);
         }
+    }
+    
+    IEnumerator AnimateLaser(Vector3 targetPosition, Vector3 targetScale, float duration)
+    {
+        float time = 0;
+        Vector3 initialPosition = laser.transform.localPosition;
+        Vector3 intialScale = laser.transform.localScale;
+
+        while (time < duration)
+        {
+            time += Time.deltaTime;
+            float t = time/duration;
+            laser.transform.localPosition = Vector3.Lerp(initialPosition, targetPosition, t);
+            laser.transform.localScale = Vector3.Lerp(intialScale, targetScale, t);
+            yield return null;
+        }
+
+        // Make sure final pos and scale are set
+        laser.transform.localPosition = targetPosition;
+        laser.transform.localScale = targetScale;
     }
 }
