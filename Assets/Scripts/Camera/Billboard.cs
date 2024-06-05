d27784191b639369d345fd813d943865740c6f36
Ernani Raffo <ernani.raffo@icloud.com>
2024-06-04 22:27:41 -0700

    Enabled map UI in every level except Level 15

--- Assets/Scripts/Camera/Billboard.cs
+++ Assets/Scripts/Camera/Billboard.cs
@@ -12,4 +12,12 @@ public class Billboard : MonoBehaviour
         transform.position = follow.position + new Vector3(0, 5, 0);
         transform.LookAt(transform.position + cam.forward);
     }
+
+    void Update() {
+        if (!PlayerManager.instance.CompanionMode && follow == PlayerManager.instance.playerTwo.transform) {
+            gameObject.SetActive(false);
+        } else if (PlayerManager.instance.CompanionMode && follow == PlayerManager.instance.playerTwo.transform) {
+            gameObject.SetActive(true);
+        }
+    }
 }
